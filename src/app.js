52f636787b4563d3696e156047277b9fb030f06d
budbro802 <and6084@hotmail.com>
2024-12-15 14:23:31 -0500

    Switching to streets-relief-vector

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
 
 // Create the map
 const map = new Map({
-  basemap: "arcgis-streets",
+  basemap: "streets-relief-vector",
 });
 
 // Create a MapView instance and link it to the #viewDiv element
