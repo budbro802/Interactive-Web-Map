69276620d75421b6dcb2666f52994294a2ae1c4a
budbro802 <and6084@hotmail.com>
2024-12-15 14:07:58 -0500

    Switch to arcgis streets part 2

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
 
 // Create the map
 const map = new Map({
-  basemap: "arcgis-navigation",
+  basemap: "arcgis-streets",
 });
 
 // Create a MapView instance and link it to the #viewDiv element
